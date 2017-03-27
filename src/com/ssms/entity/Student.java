dc05be497ee391f9920bc030ae9d948ed1489025
Jaydeep Ranipa <jaydeep.ranipa@microfocus.com>
2017-03-27 17:52:22 +0530

    create dynamic-web-project in eclipse and create pojo Student and Subject

---
+++ src/com/ssms/entity/Student.java
@@ -0,0 +1,26 @@
+package com.ssms.entity;
+
+public class Student {
+	private int id;
+	private String name;
+	private String city;
+	
+	public int getStudentId() {
+		return id;
+	}
+	public void setStudentId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}	
+}
