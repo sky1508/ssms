464c8720ee2c52bc2e1dfe5e1ebe255edca2d7e6
Jaydeep Ranipa <jaydeep.ranipa@microfocus.com>
2017-04-14 18:25:50 +0530

    tomcat starts :)

--- src/com/ssms/controller/AddStudent.java
+++ src/com/ssms/controller/AddStudent.java
@@ -14,7 +14,7 @@ import com.ssms.util.LogPrinter;
 /**
  * Servlet implementation class AddStudent
  */
-@WebServlet("AddStudent")
+@WebServlet("/AddStudent")
 public class AddStudent extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
